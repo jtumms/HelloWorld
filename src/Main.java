744010cc0636ad6716cc7c046255affd92e8e539
jtumms <john.tumminelli@tummsmedia.com>
2016-09-15 11:19:54 -0400

    additional file due to problems with constructor

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by john.tumminelli on 9/14/16.
  */
@@ -24,6 +27,30 @@ public class Main {
         for (String name : names) {
             System.out.println(name);
         }
+
+        ArrayList<String> names2 = new ArrayList(); //<String> generic - arrayList can only contain strings
+        names2.add("ALice");
+        names2.add("Bob");
+        names2.add("Charlie");
+        System.out.println(names2.get(0)); //get method
+        names2.remove(1);
+        System.out.println(names2); //returns  out entire arraylist
+
+        HashMap<String, Object> person = new HashMap();
+        person.put("name", "Alice");
+        person.put("age", 30);
+        person.put("location", "Charleston");
+        System.out.println(person);
+        System.out.println(person.get("name"));
+        person.put("name", "Bob");
+        System.out.println(person.get("name"));
+
+        String alice = names2.get(0);
+        String bob = (String) person.get("name");
+
+
+
+
 
     }
 }
