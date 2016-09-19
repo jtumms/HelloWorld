523c346e7a9619ec3133494c0c04a55f9164bb1e
jtumms <john.tumminelli@tummsmedia.com>
2016-09-19 00:39:03 -0400

    Add HashMap Example - See IY github for full version

--- src/HashMapStuff.java
+++ src/HashMapStuff.java
@@ -1,25 +1,23 @@
+/**
+ * Created by john.tumminelli on 9/19/16.
+ */
+
 import java.util.HashMap;
 import java.util.Scanner;
 
-/**
- * Created by john.tumminelli on 9/16/16.
- */
+
+
 public class HashMapStuff {
-
-    static HashMap<String, Person> users = new HashMap<>();
-
     public static void main(String[] args) {
         users.put("Alice", new Person("Alice", 30, true));
-        users.put("Bob". new Person("Bob", 30, true));
+        users.put("Bob", new Person("Bob", 40, true));
 
         Scanner scanner = new Scanner(System.in);
 
         boolean keepRunning = true;
         while (keepRunning) {
-
-
             System.out.println("Enter username:");
-            String name = scanner.nextline();
+            String name = scanner.nextLine();
 
             if (!users.containsKey(name)) {
                 Person p = new Person(name, 20, true);
@@ -28,13 +26,10 @@ public class HashMapStuff {
 
             Person person = users.get(name);
 
-            boolean loggedIn = true;
-            while (loggedIn) {
-                System.out.prinln("1 - Check balnace");
-                System.out.println("");
-            }
+            System.out.println("Logged in as: " + person.name);
         }
-
-
     }
+
+
+    static HashMap<String, Person> users = new HashMap<>();
 }
